d63d29d6f868dce663634c50c98418c50c5032e8
geninhocell <geninhocell@gmail.com>
2023-01-28 14:57:20 -0300

    fix: shared

--- node.js
+++ node.js
@@ -1,42 +1,6 @@
 module.exports = {
+  extends: ['./shared/base.js'],
   env: {
-    es2021: true,
     node: true
   },
-  extends: [
-    'standard',
-    'plugin:prettier/recommended',
-    "plugin:@typescript-eslint/recommended"
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    "@typescript-eslint"
-  ],
-  rules: {
-    'prettier/prettier': ["error", {
-      'printWidth': 80,
-      'tabWidth': 2,
-      'singleQuote': true,
-      'trailingComma': 'all',
-      'arrowParens': 'always',
-      'semi': false,
-      'endOfLine': 'auto',
-    }],
-    "import/extensions": [
-      error,
-      ignorePackages,
-      {
-        ts: never
-      }
-    ]
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {}
-    }
-  }
 }
